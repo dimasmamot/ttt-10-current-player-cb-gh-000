925d8cdadd7b4139aa369d3dc5c8aa1a23e8d320
Dimas Rizky <dimas.rizky.hp@gmail.com>
2018-11-14 13:18:40 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,8 +12,8 @@ end
 def current_player(board)
   step = turn_count(board)
   if step % 2 == 0
-    return "X"
-  elsif step == 0 || step % 2 == 1
     return "O"
+  elsif step == 0 || step % 2 == 1
+    return "X"
   end
 end
