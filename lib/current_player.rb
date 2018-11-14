6247dc8bda460a181cb49a73dbb219556f63b82c
Dimas Rizky <dimas.rizky.hp@gmail.com>
2018-11-14 13:18:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  count = 1
+  count = 0
   board.each do |item|
     if item == "O" || item == "X"
       count += 1
@@ -12,8 +12,8 @@ end
 def current_player(board)
   step = turn_count(board)
   if step % 2 == 0
-    return "O"
-  elsif step == 0 || step % 2 == 1
     return "X"
+  elsif step == 0 || step % 2 == 1
+    return "O"
   end
 end
